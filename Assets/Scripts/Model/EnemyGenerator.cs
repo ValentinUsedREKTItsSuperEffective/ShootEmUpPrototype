752ca4b0e90b9332005088af3f883f19479e8826
Valentin Bonnesoeur <valentin.bonnes@gmail.com>
2019-02-11 23:57:33 +0900

    Place enemy randomly

---
+++ Assets/Scripts/Model/EnemyGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour {
+
+    public GameObject enemyPlanetCenterPrefab;
+    public GameObject player;
+    public GameObject planet;
+
+	// Use this for initialization
+	void Start () {
+        GameObject enemyPivot = Instantiate (enemyPlanetCenterPrefab);
+        enemyPivot.transform.parent = planet.transform;
+        Enemy enemy = enemyPivot.transform.Find ("Enemy").GetComponent<Enemy>();
+        enemy.player = player;
+        enemyPivot.transform.Rotate (new Vector3 (0, 0, Random.Range(-12, 12)));
+	}
+}
