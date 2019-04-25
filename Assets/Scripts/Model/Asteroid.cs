3539770356b7b5d6a2c4ec27cf44e91f239f0c00
Valentin Bonnesoeur <valentin.bonnes@gmail.com>
2019-04-25 11:30:08 +0900

    Add some invulnerability to the asteroids

--- Assets/Scripts/Model/Asteroid.cs
+++ Assets/Scripts/Model/Asteroid.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 
 public class Asteroid : Enemy {
-
+    
     public override void Generate() {
         Projectile projectile = gameObject.GetComponent<Projectile> ();
         Vector3 direction = transform.parent.position - transform.position;
         direction.Normalize ();
         transform.position -= 4 * direction;
         projectile.InitProjectile (model.damage, model.speed, direction, tag);
+    }
+
+    public override void Hit(int damage) {
+        // Invulnerability
+        if (transform.position.y > 10) {
+            return;
+        }
+
+        base.Hit (damage);
     }
 }
