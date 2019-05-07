fa438e8b89994f1a600caee72afefb11d62761b5
Valentin Bonnesoeur <valentin.bonnes@gmail.com>
2019-05-07 15:13:00 +0900

    Add a game over when the planet loose to many life.

--- Assets/Scripts/Model/Planet.cs
+++ Assets/Scripts/Model/Planet.cs
@@ -1,7 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Planet : BaseEntity {
-    
+
+    public float threshold;
+
+    public override void Hit(int damage) {
+        base.Hit (damage);
+
+        if(model.currentLife.Value < model.life * threshold){
+            model.currentLife.Value = 0;
+        }
+    }
 }
--- Assets/Scripts/View/GameOver.cs
+++ Assets/Scripts/View/GameOver.cs
@@ -7,12 +7,20 @@ public class GameOver : MonoBehaviour {
     [SerializeField] EntityModel playerModel;
     ReactiveProperty<bool> playerDead;
 
+    [SerializeField] EntityModel homeModel;
+    ReactiveProperty<bool> homeDestroyed;
+
     [SerializeField] Text gameOverText;
 
 	void Start () {
         playerDead = playerModel.currentLife.Select (life => life <= 0).ToReactiveProperty ();
+        homeDestroyed = homeModel.currentLife.Select (life => life <= 0).ToReactiveProperty ();
 
         playerDead.Where(isDead => isDead == true).Subscribe (_ => {
+            gameOverText.gameObject.SetActive (true);
+        });
+
+        homeDestroyed.Where (isDestroyed => isDestroyed == true).Subscribe (_ => {
             gameOverText.gameObject.SetActive (true);
         });
 	}
